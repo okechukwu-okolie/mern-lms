8f4db815c1f66e1b9f0ed2ec38b2b61133230c7e
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-11-03 16:29:37 +0100

    adding to the codebase

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const authRoutes = require('./routes/auth-routes/index')
 
 
 
@@ -30,6 +31,9 @@ mongoose.connect(MONGO_URL)
 
 
 //route configuration
+
+app.use('/auth', authRoutes)
+
 
 app.use((err,req,res,next)=>{
     console.log(err.stack);
