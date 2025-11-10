7671ac2a405a24a9b937a718369bd26101d08e5b
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-11-10 16:29:25 +0100

    adding to the codebase

--- client/src/components/instructor-view/courses/index.jsx
+++ client/src/components/instructor-view/courses/index.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { X } from "lucide-react";
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const InstructorCourses = () => {
   return (
-    <div>
-      InstructorCourses
-    </div>
-  )
-}
+    <Card>
+      <CardHeader classname="flex justify-between flex-row items-center">
+        <CardTitle className="text-3xl font-extrabold">All Courses</CardTitle>
+        <Button className="p-6">Create New Course</Button>
+      </CardHeader>
+      <CardContent>
+        <div classname="overflow-x-auto">
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Course</TableHead>
+                <TableHead>Students</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default InstructorCourses
+export default InstructorCourses;
